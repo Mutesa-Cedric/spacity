c77279aedf56560023cb01a0e5243908712d1363
Mutesa-Cedric <mutesacedric@gmail.com>
2022-06-10 10:21:17 +0200

    update : finished homepage basics

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,36 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Navbar from '../components/navbar'
+import Fade from 'react-reveal/Fade';
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="overflow-hidden">
       <Head>
-        <title>Spacity</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Spacity - Home</title>
+        <link rel="icon" href="/assets/shared/logo.svg" />
       </Head>
+      <main className="flex relative w-screen h-screen flex-col  bg-center bg-cover bg-no-repeat"
+        style={{ backgroundImage: 'url(/assets/home/background-home-desktop.jpg)' }}>
+        <Navbar currentPage='home' />
+        <div className=' absolute top-[40%] px-52 w-full h-3/5
+        flex justify-between items-center pb-10'>
+          <div className='flex flex-col justify-around h-auto '>
+            <Fade left duration={2000}>
+              <p className='text-[#D0D6F9] font-["lato",sans-serif]'>SO, YOU WANT TO TRAVEL TO</p>
+              <p id='space-text' className='text-white leading-10 text-[172px]'>SPACE</p>
+              <p id='home-desc'>Let&apos;s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we&apos;ll give you a truly out of this world experience!</p>
+            </Fade>
+          </div>
+          <Fade bottom duration={3000} >
+            <div className=' cursor-pointer transition duration-200 ease-out hover:scale-150 h-[48%] mb-4 self-end w-[32%] flex items-center justify-center rounded-[50%] bg-white'>
+              <span id='explore-text'> EXPLORE</span>
+            </div>
+          </Fade>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-
+        </div>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-
-      </footer>
     </div>
   )
 }
