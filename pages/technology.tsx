98430c3becb54c6fc9d22ace7eb0a2ef2ad6eb6e
Mutesa-Cedric <mutesacedric@gmail.com>
2022-06-11 00:49:51 +0200

    update : finished technology page basics

--- pages/technology.tsx
+++ pages/technology.tsx
@@ -1,16 +1,34 @@
 import Head from 'next/head'
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/navbar'
+import TechComponent from '../components/tech-component'
 
 function Technology() {
+  const [currentTech, setCurrentTech] = useState('Launch vehicle')
+
+  //updating currentcrew when user clicks on a crew
+
+  const updateTech = (tech: string) => {
+    setCurrentTech(tech)
+    // console.log("changing on destination click");
+  }
   return (
     <div>
       <Head>
         <title>Spacity - Technology</title>
         <link rel="icon" href="/assets/shared/logo.svg" />
       </Head>
-      <main>
-        <Navbar currentPage='technology' />
+      <Navbar currentPage='technology' />
+
+      <main className="flex relative w-screen h-screen flex-col  bg-center bg-cover bg-no-repeat"
+        style={{ backgroundImage: 'url(/assets/technology/background-technology-desktop.jpg)' }}>
+        <div className='absolute top-[23%] pl-52 pb-16 w-full h-[77%]  flex flex-col justify-between px-50'>
+        <div className='flex items-center'>
+            <span className='text-white opacity-25 font-bold text-[28px] leading-[34px] tracking-[4.725px] mr-4'>03</span>
+            <span className='font-light text-[28px] leading-[34px] tracking-[4.725px] text-white uppercase'>SPACE LAUNCH 101</span>
+          </div>
+          <TechComponent currentTech={currentTech} updateTech={updateTech}/>
+        </div>
       </main>
     </div>
   )
