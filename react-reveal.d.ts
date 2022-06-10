84c5dc390a956bc0c9ea87b8b2fcc248f3ceb779
Mutesa-Cedric <mutesacedric@gmail.com>
2022-06-10 10:20:16 +0200

    ts module declaration of react-reveal

---
+++ react-reveal.d.ts
@@ -0,0 +1,7 @@
+declare module 'react-reveal/Fade';
+declare module 'react-reveal/Flip';
+declare module 'react-reveal/Slide';
+declare module 'react-reveal/Zoom';
+declare module 'react-reveal/Bounce';
+declare module 'react-reveal/Rotate';
+declare module 'react-reveal/Roll';
