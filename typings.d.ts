423fd96eb91d50b562401d53e1d24490aa2b90e8
Mutesa-Cedric <mutesacedric@gmail.com>
2022-06-10 18:00:20 +0200

    feature: module declarations

---
+++ typings.d.ts
@@ -0,0 +1,29 @@
+export interface Destination{
+    name: string;
+    images:{
+        png: string;
+        webp: string;
+    };
+    description:string;
+    distance:string;
+    travel:string;
+}
+
+export interface Crew{
+    name:string;
+    images:{
+        png: string;
+        webp: string;
+    };
+    role:string;
+    bio:string;
+}
+
+export interface Technology{
+    name:string;
+    images:{
+        png: string;
+        webp: string;
+    };
+    description:string;
+}
